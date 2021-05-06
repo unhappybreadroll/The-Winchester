ce83bb8b789ab39468d675566671855b8b169c0a
unhappybreadroll <samdoherty007@gmail.com>
2021-05-06 16:03:18 +0100

    3rd commit - animated character and fixed camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+
+    private Transform PlayerTransform;
+
+
+   
+    void Start()
+    {
+        PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+   
+    void lateUpdate()
+    {
+        // storing current cameras position in variable temp
+        Vector2 temp = transform.position;
+        // set the cameras position x to be equal to the position x
+        temp.x = PlayerTransform.position.x;
+        // set back the cameras temp position to the cameras current position
+        transform.position = temp;
+
+        // reason to do it this way is because you can edit the players temporary position
+    }
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+}

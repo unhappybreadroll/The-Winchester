3f8ed3cee4251756af30620ef166a2c7203c23b8
unhappybreadroll <samdoherty007@gmail.com>
2021-05-20 16:21:19 +0100

    mad some fixes, added zombies and programmed them and audio

---
+++ Assets/zombie.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float range;
+    [SerializeField]
+    float maxDiatance;
+
+    Vector2 wayPoint;
+    
+
+    
+
+
+    void Start()
+    {
+        SetNewDestination();
+    }
+
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint, speed * Time.deltaTime);
+        if(Vector2.Distance(transform.position,wayPoint) < range)
+        {
+            SetNewDestination();
+        }
+    }
+
+    void SetNewDestination()
+    {
+        wayPoint = new Vector2(Random.Range(maxDiatance, maxDiatance), Random.Range(maxDiatance, maxDiatance));
+    }
+
+
+
+
+
+}
